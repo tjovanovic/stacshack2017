541a424bd60e9668ff4b7f749de4a94b4cdbb12a
dean <saadouni85@gmail.com>
2017-03-05 07:56:09 +0000

    can now request as many songs as we wish

--- src/lyrics.py
+++ src/lyrics.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 import html
 import pickle
 
-# import local_settings
+import local_settings
 
 
 logger = logging.getLogger(__name__)
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 LYRICS_URL_FORMAT = 'http://lyrics.wikia.com/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles={}:{}'
 HELP_URL_FORMAT = 'http://lyrics.wikia.com/api.php?action=lyrics&func=getSong&fmt=xml&artist={}&song={}'
-TOP_URL_FORMAT = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&limit={}&api_key={}'
+TOP_URL_FORMAT = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&limit={}&api_key={}&page={}'
 
 
 lyrics_pattern = re.compile(r'&lt;lyrics&gt;(.*)&lt;/lyrics&gt;', re.DOTALL)
@@ -67,16 +67,33 @@ def fetch_lyrics(artist, title):
     return lines
 
 
-def fetch_top(num=500):
-    url = TOP_URL_FORMAT.format(num, local_settings.LASTFM_API_KEY)
-    text = fetch_url(url)
+def fetch_top(num):
+
+    pages = num//1000
+    reminaing = num - pages*1000
 
     top = []
-    tree = ET.fromstring(text)
-    for track in tree[0]:
-        title = track.find('name').text
-        artist = track.find('artist').find('name').text
-        top.append((artist, title))
+    for page in range(pages):
+        url = TOP_URL_FORMAT.format(1000, local_settings.LASTFM_API_KEY, page+1)
+        text = fetch_url(url)
+
+        tree = ET.fromstring(text)
+        for track in tree[0]:
+            title = track.find('name').text
+            artist = track.find('artist').find('name').text
+            top.append((artist, title))
+
+    if reminaing > 0:
+        url = TOP_URL_FORMAT.format(reminaing, local_settings.LASTFM_API_KEY, pages+1)
+        text = fetch_url(url)
+
+        tree = ET.fromstring(text)
+        for track in tree[0]:
+            title = track.find('name').text
+            artist = track.find('artist').find('name').text
+            top.append((artist, title))
+
+
     return top
 
 
@@ -105,8 +122,16 @@ def read_songs():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(message)s')
     # save_songs()
-    next_line, artist_song = read_songs()
-    for bla in next_line.keys():
-        print(bla)
+    
+    # next_line, artist_song = read_songs()
+    top_5000 = fetch_top(5000)
+    for artist, title in top_5000:
+        lyrics = fetch_lyrics(artist, title)
+    
+    # for bla in next_line.keys():
+    #     print(bla)
+    
+
+
     # print(next_line.keys())
     # print(artist_song)
--- src/song_alexa.py
+++ src/song_alexa.py
@@ -21,14 +21,43 @@ def init():
 
 
 @ask.intent("LoveIntent")
-def love(line):
+def love():
     return question('I love you too babe')
 
+@ask.intent("SetModeIntent", convert={'first':str})
+def set_mode(first):
 
-@ask.intent("MatchIntent", convert={'first': str})
+	if first == 'find song name':
+		session.attribute['mode'] = 1
+		msg = 'mode set to find the song name and artsit'
+
+	elif first == 'find next line':
+		session.attribute['mode'] = 2
+		msg = 'mode set to find the next line'
+
+	elif first == 'find audio':
+		session.attribute['mode'] = 3
+		msg = 'mode set to find audio'
+
+	else:
+		session.attribute['mode'] = 1
+		msg = 'mode not regonized, default find song name set'
+
+	return question(msg)
+
+
+
+
+
+@ask.intent("AskIntent", convert={'first':str})
+def ask():
+	return 0
+
+
+
 def match(first):
-    next_line, artist_song = read_songs()
 
+    next_line, artist_song = read_songs()
     query = first.lower()
     q = min(artist_song.keys(), key=lambda x: edit_distance(x, query))
     dist = edit_distance(q, query)
@@ -38,5 +67,28 @@ def match(first):
     return question(str(artist_song[q][0]) + ': ' + str(artist_song[q][1]))
 
 
+
+def next_line(first):
+
+	next_line, artist_song = read_songs()
+	msg = next_line[first]
+	return question(msg)
+
+# @ask.intent("YoutubeIntent", convert={'first':str})
+# def youtube():
+# 	return 0
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+"""
+Modes:
+
+1. state artist name, song name (done)
+2. sing next line (doing)
+3. play song (youtube?)
+"""
